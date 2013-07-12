f53d96b50f5fa70f719c22e27b7f5d2cee1c574e
Michel Daviot <michel.daviot@atos.net>
2013-07-12 18:16:05 +0200

    fixed project, now compiles with Maven (still issues with Eclipse)

--- macros/src/main/scala/Macros.scala
+++ macros/src/main/scala/Macros.scala
@@ -2,6 +2,11 @@ import language.experimental.macros
 import scala.reflect.macros.Context
 
 object Macros {
-  def impl(c: Context) = c.universe.reify(println("hello world!"))
   def hello = macro impl
+  def impl(c: Context) = {
+    // uses the technique described in:
+    // http://docs.scala-lang.org/overviews/macros/overview.html#writing_bigger_macros
+    val helper = new Helper[c.type](c)
+    c.Expr[Unit](helper.hello)
+  }
 }
